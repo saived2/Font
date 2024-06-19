f7d135729c4940abd140f671dae393b8efd0a117
saived2 <137843759+saived2@users.noreply.github.com>
2024-06-18 20:25:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+difference = 0; 
+rightWristX = 0; 
+leftWristX = 0;
+
+
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -8,10 +14,14 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 } 
-
+ 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+       leftWristX = results[0].pose.leftWrist.x;
+       rightWristX = results[0].pose.rightWrist.x;
+       difference = floor(leftWristX-rightWristX);
+
     }
 }
 
@@ -21,5 +31,8 @@ function modelLoaded() {
 
 function draw (){
     background('8FFFDC');
+    textSize(difference);
+    fill('#BCF4FF');
+    text('Saisha',50,400);
 
 }
